74563700a6af0905a20467923313267db9e8cd93
rkp <ofaryfacta7133@gmail.com>
2023-03-17 14:11:31 +0530

    color color 1

--- colors.sh
+++ colors.sh
@@ -3,4 +3,4 @@
 echo -e "\e[32m I am printing Green Slot \e[0m" 
 echo -e "\e[31m I am printing Red Slot \e[0m"
 echo -e "\e[33m I am printing Yellow Slot \e[0m"
-echo -e "\e[34m I am printing Cyan Slot \e[0m"
+echo -e "\e[36m I am printing Cyan Slot \e[0m" 
