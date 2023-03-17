7f2830d7d15574babd32b81ca3ef8768995f9133
rkp <ofaryfacta7133@gmail.com>
2023-03-17 23:25:08 +0530

    functions

--- read.sh
+++ read.sh
@@ -2,3 +2,11 @@
 
 read -p 'enter your name:' name
 echo -e "entered name is \e[33m $name \e[0m"
+
+#functions : used to use set of commands
+
+sample() {
+    echo "I am Sample Function"
+}
+ 
+sample 
