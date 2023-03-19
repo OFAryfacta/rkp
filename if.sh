c466a75c6c5af06b4ef70dd69a20ca9726a241ac
rkp <ofaryfacta7133@gmail.com>
2023-03-19 19:12:46 +0530

    if122

--- if.sh
+++ if.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 a=$1
 
+# if [ "$a" == "xyz" ]; then
+#     echo "both of the are real"
+# fi  
+
 if [ "$a" == "xyz" ]; then
-    echo "both of the are real"
-fi  
+   echo "both of them are equal" 
+else 
+    echo "both of them are not equal"
+fi       
