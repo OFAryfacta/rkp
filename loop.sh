93244c793019838908fec0a39fd0e9811d75ab43
rkp <ofaryfacta7133@gmail.com>
2023-03-19 20:25:04 +0530

    loop/while

--- loop.sh
+++ loop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-for list in val1 val2 val3 val4 ; do
-    echo Value is $list
-done     
+# for list in val1 val2 val3 val4 ; do
+#     echo Value is $list
+# done     
+
+val=0
+while [ $val -lt 10 ]; do
+   echo "I = $val"
+   i=$(($val+1))
+done   
