93105b605dd86a29b39a6a21fba619afec341aeb
rkp <ofaryfacta7133@gmail.com>
2023-03-19 16:45:46 +0530

    with exit codes

--- case.sh
+++ case.sh
@@ -10,14 +10,19 @@ ACTION=$1
 case $ACTION in
     start)
         echo "XYZ Service is Starting"  
+        exit 0
        ;;
     stop)
         echo "XYZ Service is Stopping"
+        exit 0
         ;;
     restart)
         echo "XYZ service is restarting"
-        ;;        
-
+        exit 0
+        ;;
+    *)
+    echo "not a valid option"
+    exit 1                 
 esac
 
 #hello
