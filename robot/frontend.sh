c9975044a1d54f1f3027e0a63b7f4d9771501760
rkp <ofaryfacta7133@gmail.com>
2023-03-22 17:39:11 +0530

    fe

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -19,7 +19,7 @@ systemctl enable nginx  &>> /tmp/frontend.log
 systemctl start nginx   &>> /tmp/frontend.log
 stat $?
 
-echo -n "Downloading the $COMPONENT "
+echo -n "Downloading the $COMPONENT :"
 curl -s -l -o /tmp/$COMPONENT.ZIP "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
         
