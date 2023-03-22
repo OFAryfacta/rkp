c40d7b6d220d2f79dac582c2233864aae4561b49
rkp <ofaryfacta7133@gmail.com>
2023-03-22 16:41:44 +0530

    fe

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "I am Frontend"
 
-yum install nginx -y    &>> /tmp/frontend.log
-systemct1 enable nginx  &>> /tmp/frontend.log
-systemct1 start nginx   &>> /tmp/frontend.log
+yum install nginx -y    
+systemct1 enable nginx  
+systemct1 start nginx  
