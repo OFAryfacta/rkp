9f779be82aece96e9eb883dfc6508bf28d444d6e
rkp <ofaryfacta7133@gmail.com>
2023-03-22 17:01:28 +0530

    fe

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+echo "installing nginx :"
 yum install nginx -y    &>> /tmp/frontend.log
 systemctl enable nginx  &>> /tmp/frontend.log
 systemctl start nginx   &>> /tmp/frontend.log
+
+echo "starting Nginx :"
+systemctl start nginx &>> /tmp/frontend.log 
