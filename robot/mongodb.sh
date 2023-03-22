aacce13fe94e60c16dffe2ebff2336a6d9cde0a7
rkp <ofaryfacta7133@gmail.com>
2023-03-22 20:46:22 +0530

    makefile

--- robot/mongodb.sh
+++ robot/mongodb.sh
@@ -11,9 +11,9 @@ fi
 
 stat(){
     if [ $1 -eq 0 ]; then
-       echo -e "\e[32 Success \e[0m"
+       echo -e "\e[32m Success \e[0m"
     else 
-       echo -e "\e[32 Failure \e[0m"
+       echo -e "\e[32m Failure \e[0m"
 fi          
 }
 
