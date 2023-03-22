b2d1ac7e9331ae7eb39a69e9dbfe57572b445214
rkp <ofaryfacta7133@gmail.com>
2023-03-22 20:07:28 +0530

    makefile

--- robot/mongodb.sh
+++ robot/mongodb.sh
@@ -1,3 +1,48 @@
 #!/bin/bash
+set -e
 
-echo "i am mongoDb"
+COMPONENT=mongodb
+LOGFILE=/tmp/$COMPONENT.log
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+   echo -e "\e[31m You need to script either as a root user or with a sudo privilege \e[0m"
+   exit 1
+fi
+
+stat(){
+    if [ $1 -eq 0 ]; then
+       echo -e "\e[32 Success \e[0m"
+    else 
+       echo -e "\e[32 Failure \e[0m"
+fi          
+}
+
+echo -n "Downloading $COMPONENT : "
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+echo -n "Installing $COMPONENT : "
+yum install -y mongodb-org   &>> $LOGFILE
+
+echo -n "Starting $COMPONENT : "
+systemctl enable mongodb
+systemctl start mongodb
+stat $?
+
+echo -n "downloading the $COMPONENT schema :"
+curl -s -l -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $?
+
+echo -n "extracting the $COMPONENT schema files : "
+cd /tmp
+unzip mongodb.zip &>> $LOGFILE
+stat $?
+
+echo -n "Inecting the schema : "
+cd mongodb-main
+mongo < catalogue.js &>>$LOGFILE
+mongo < user.js      &>>$LOGFILE
+stat() $?
+
+
