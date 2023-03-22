b880530bf1460f17836717668af7afd8e47af6c6
rkp <ofaryfacta7133@gmail.com>
2023-03-22 17:36:51 +0530

    fe

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+cCOMPONENT=frontend
 
-echo -n "installing nginx :"
-yum install nginx -y    &>> /tmp/frontend.log
-systemctl enable nginx  &>> /tmp/frontend.log
-systemctl start nginx   &>> /tmp/frontend.log
-if [ $? -eq 0 ]; then
-    echo "Success"
-else 
-    echo "Failure"
-fi         
-
-echo -n "starting Nginx :"
-systemctl start nginx &>> /tmp/frontend.log 
-if [ $? -eq 0 ]; then
+stat() {
+    if [ $1 -eq 0 ]; then
     echo "Success"
 else 
     echo "Failure"
 fi 
+}
+
+echo -n "installing nginx :"
+yum install nginx -y    &>> /tmp/frontend.log
+stat $?
+
+echo -n "Starting Nginx :"
+systemctl enable nginx  &>> /tmp/frontend.log
+systemctl start nginx   &>> /tmp/frontend.log
+stat $?
+
+echo -n "Downloading the $COMPONENT "
+curl -s -l -o /tmp/$COMPONENT.ZIP "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+        
+
