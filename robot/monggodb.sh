d167754c89f82ed121ed6e60efea7ef5d42d8427
rkp <ofaryfacta7133@gmail.com>
2023-03-24 16:18:37 +0530

    makefile

--- robot/monggodb.sh
+++ robot/monggodb.sh
@@ -19,24 +19,24 @@ stat $?
 echo -n "Installing $COMPONENT : "
 yum install -y mongodb-org  &>> $LOGFILE
 
-echo -n "Starting $COMPONENT : "
-systemctl enable mongod
-systemctl start mongod
-stat $?
+# echo -n "Starting $COMPONENT : "
+# systemctl enable mongod
+# systemctl start mongod
+# stat $?
 
-echo -n "downloading the $COMPONENT schema :"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-stat $?
+# echo -n "downloading the $COMPONENT schema :"
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+# stat $?
 
-echo -n "extracting the $COMPONENT schema files : "
-cd /tmp
-unzip -o mongodb.zip &>> $LOGFILE
-stat $?
+# echo -n "extracting the $COMPONENT schema files : "
+# cd /tmp
+# unzip -o mongodb.zip &>> $LOGFILE
+# stat $?
 
-echo -n "Injecting the schema : "
-cd mongodb-main
-mongo < catalogue.js &>>$LOGFILE
-mongo < user.js      &>>$LOGFILE
-stat $?
+# echo -n "Injecting the schema : "
+# cd mongodb-main
+# mongo < catalogue.js &>>$LOGFILE
+# mongo < user.js      &>>$LOGFILE
+# stat $?
 
 
