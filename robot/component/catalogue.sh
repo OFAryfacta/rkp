57364fd06fedf661a9e0f2ee011b3674a3783d7a
rkp <ofaryfacta7133@gmail.com>
2023-03-25 14:18:16 +0530

    makefile

--- robot/component/catalogue.sh
+++ robot/component/catalogue.sh
@@ -6,8 +6,8 @@ COMPONENT==catalogue
 source robot/component/common.sh
 
 echo -n "Configuring NodeJS Repo : "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y 
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE
+yum install nodejs -y &>> $LOGFILE
 stat $?
 
 echo -n "Configuring Application User $APPUSER"
