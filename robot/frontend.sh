bb49581cb3307969d074e16a72b3333b1355e5a6
rkp <ofaryfacta7133@gmail.com>
2023-03-25 13:16:33 +0530

    makefile

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -4,13 +4,7 @@ set -e ""
 
 COMPONENT=frontend
 
-stat() {
-    if [ $1 -eq 0 ]; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-fi 
-}
+source common.sh
 
 echo -e  "\e[32m ____________$COMPONENT Configuration Starting ___________\e[0m"
 
