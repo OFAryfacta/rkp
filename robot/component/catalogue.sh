f0c649f6b0ff822ab0c20015d9556e74410f385a
rkp <ofaryfacta7133@gmail.com>
2023-03-28 18:13:44 +0530

    makefile

--- robot/component/catalogue.sh
+++ robot/component/catalogue.sh
@@ -9,9 +9,12 @@ curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>
 yum install nodejs -y &>> "${LOGFILE}"
 stat $?
 
-echo -n "Configuring Application User $APPUSER : "
-useradd $APPUSER &>> "${LOGFILE}"
-stat $?
+id $APPUSER &>> "${LOGFILE}"
+if [ $? -ne 0 ]; then
+    echo -n "Creating Application User $APPUSER : "
+    useradd $APPUSER &>> "${LOGFILE}"
+    stat $?
+fi
 
 # echo -n "Downloading the $COMPONENT"
 # curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
