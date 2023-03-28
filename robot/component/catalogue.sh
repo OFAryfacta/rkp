b2dc0f73279de449193cc3d76338a8a1181e6958
rkp <ofaryfacta7133@gmail.com>
2023-03-28 19:01:58 +0530

    makefile

--- robot/component/catalogue.sh
+++ robot/component/catalogue.sh
@@ -28,5 +28,5 @@ stat $?
 
 echo -n "changing the ownership to $APPUSER "
 mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
-chown $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
+chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
 stat $?
