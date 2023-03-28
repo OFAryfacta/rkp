0c574334efbb05392e9a6a1253832b46c97cfded
rkp <ofaryfacta7133@gmail.com>
2023-03-28 17:48:39 +0530

    makefile

--- robot/component/catalogue.sh
+++ robot/component/catalogue.sh
@@ -9,7 +9,7 @@ curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>
 yum install nodejs -y &>> "${LOGFILE}"
 stat $?
 
-echo -n "Configuring Application User $APPUSER"
+echo -n "Configuring Application User $APPUSER : "
 useradd $APPUSER &>> "${LOGFILE}"
 stat $?
 
