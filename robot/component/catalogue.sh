1577c1e23b7f8f9bcf59c5cab27888cef14b4827
rkp <ofaryfacta7133@gmail.com>
2023-03-28 17:52:11 +0530

    makefile

--- robot/component/catalogue.sh
+++ robot/component/catalogue.sh
@@ -13,10 +13,10 @@ echo -n "Configuring Application User $APPUSER : "
 useradd $APPUSER &>> "${LOGFILE}"
 stat $?
 
-echo -n "Downloading the $COMPONENT"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
-stat $?
+# echo -n "Downloading the $COMPONENT"
+# curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+# stat $?
 
-echo -n "Extracting the $COMPONENT : "
-cd /home/$APPUSER
-unzip -o /tmp/$COMPONENT.zip &>> "${LOGFILE}"
+# echo -n "Extracting the $COMPONENT : "
+# cd /home/$APPUSER
+# unzip -o /tmp/$COMPONENT.zip &>> "${LOGFILE}"
