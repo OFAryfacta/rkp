88b8a4646549458ac6ea68efa53fb91f9e6352b3
rkp <ofaryfacta7133@gmail.com>
2023-03-28 17:50:09 +0530

    makefile

--- robot/component/common.sh
+++ robot/component/common.sh
@@ -9,8 +9,8 @@ fi
 
 stat() {
     if [ $1 -eq 0 ]; then
-       echo -e "\e[32m Stat Success \e[0m"
+       echo -e "\e[32m Success \e[0m"
     else 
-       echo -e "\e[31m Stat Failure \e[0m"
+       echo -e "\e[31m Failure \e[0m"
 fi          
 }
