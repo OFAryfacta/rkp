c61dedb482616c01a5a2bdcdd65c231b1c40eb2a
rkp <ofaryfacta7133@gmail.com>
2023-03-29 17:01:41 +0530

    makefile

--- robot/component/redis.sh
+++ robot/component/redis.sh
@@ -13,6 +13,7 @@ yum install redis -y &>> "${LOGFILE}"
 stat $?
 
 echo -n "Whitelisting the $COMPONENT : "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 stat $?
 
