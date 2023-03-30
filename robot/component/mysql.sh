324a7618fc4c04a17879ef9b7a548c26c9569172
rkp <ofaryfacta7133@gmail.com>
2023-03-30 18:28:59 +0530

    makefile

--- robot/component/mysql.sh
+++ robot/component/mysql.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
+#!/bin/bash 
 
 COMPONENT=mysql
 
-source component/common.sh
+source components/common.sh    # Source loads a file and this file has all the common patterns.
 
-echo -n "Configuring the $COMPONENT repo : "
-curl -s -L -o /etc/yum.repo.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo &>> "${LOGFILE}"
-stat $?
+echo -n "Configuring the $COMPONENT repo  : "
+curl -s -L -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo  &>> "${LOFGILE}"
+stat $? 
 
-echo -n "Installing $COMPONENT : "
-yum install mysql-community-server -y &>> "${LOGFILE}"
-stat $?
+echo -n "Installing $COMPONENT: "
+yum install mysql-community-server -y   &>> "${LOFGILE}"
+stat $? 
 
 echo -n "Starting $COMPONENT : "
 systemctl enable mysqld
-systemctl start mysqld
-stat $?
-
+systemctl start mysqld 
+stat $? 
