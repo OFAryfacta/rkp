6f67efd494e0c7045e6940494aea5fc1f5c62fea
rkp <ofaryfacta7133@gmail.com>
2023-03-30 17:43:57 +0530

    makefile

---
+++ robot/component/mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+COMPONENT=mysql
+
+source components/common.sh
+
+echo -n "COnfiguring the $COMPONENT repo : "
+curl -s -L -o /etc/yum.repo.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo &>> "${LOGFILE}"
+stat $?
+
+echo -n "Installing $COMPONENT : "
+yum install mysql-community-server -y &>> "${LOGFILE}"
+stat $?
+
+echo -n "Starting $COMPONENT : "
+systemctl enable mysqld
+systemctl start mysqld
+stat $?
+
